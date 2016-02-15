7181b6e903da3d8c1197d37a45104150f73f9597
Jacob Friis Saxberg <jacob@saxberg.dk>
2016-02-15 20:45:19 +0100

    without BUILD_ONLY

--- run.js
+++ run.js
@@ -1,1 +1,8 @@
-require('nodegit');
+var Git = require('nodegit');
+var os = require('os');
+
+Git.Clone('https://github.com/webjay/nodegit-heroku.git', os.tmpdir() + '/' + process.hrtime()[0])
+.then(function (repo) {
+  console.log(repo);
+})
+.catch(console.error);
